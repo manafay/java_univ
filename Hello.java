6c914b6b3e25cf34f8db5e9cf9fd5a65476b6af5
Abdul Nafay <nafay@flyin.com>
2017-04-27 19:09:40 +0530

    Menu Heirarchy added

--- Hello.java
+++ Hello.java
@@ -1,9 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+// import java.io.Exception;
 class Hello {
 
 public static void printChar(String val, int n) {
   for (int i = 0; i < n; ++i) {
     System.out.print(val);
   }
+}
+
+public static boolean isValidOption(int x) {
+  return x >= 1 && x <= 5;
+}
+
+public static void displayUsageMenu() {
+  System.out.println("ENTER USAGE DETAILS MENU");
+  System.out.println("Please select an option from the menu:");
+  System.out.println("1. Phone Call");
+  System.out.println("2. SMS");
+  System.out.println("3. Data Usage");
+  System.out.println("4. Return to main menu");
 }
 
 public static void displayMainMenu() {
@@ -16,7 +32,7 @@ public static void displayMainMenu() {
   System.out.println("5. Exit System");
 }
 
-  public static void main(String[] args) {
+  public static void main(String args[]) throws Exception {
     printChar("*", 80);
     System.out.println();
     printChar(" ", 15);
@@ -26,7 +42,34 @@ public static void displayMainMenu() {
     System.out.println();
     printChar("*", 80);
     System.out.println();
-    displayMainMenu();
+      displayMainMenu();
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// String s = br.readLine();
+    int i;
+    while ((i = Integer.parseInt(br.readLine())) != 5)
+    {
+      if(!isValidOption(i)) {
+          System.out.println("Value must be between 1 and 5. Please try again:");
+      } else {
+        switch(i) {
+         case 1:
+         displayUsageMenu();
+         int x;
+         while ((x = Integer.parseInt(br.readLine())) != 4)
+         {
+
+         }
+         displayMainMenu();
+            break;
+        default: break;
+        }
+      }
+
+
+    }
+
+
 }
 
 }
